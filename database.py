5c53df77f5825a61dd8239268a6533621dc45bf6
Bedirhan ASAR <bedirhanasarr06@hotmail.com>
2023-05-29 18:18:15 +0300

    Add files via upload

---
+++ database.py
@@ -0,0 +1,50 @@
+import mysql.connector
+
+cnx=mysql.connector.connect(
+    host='localhost',
+    port=3310,
+    user='root',
+    password='my_password',
+    database='my_module')
+
+cursor = cnx.cursor()
+create_table_query='''
+CREATE TABLE IF NOT EXISTS Movies(
+    ID INT,
+    MOVIE VARCHAR(50),
+    DATE VARCHAR(20),
+    MCU_PHASE VARCHAR(20))
+'''
+cursor.execute(create_table_query)
+data=[
+    ['1','IronMan','May2,2008','Phase1'],
+    ['2','TheIncredibleHulk','June13,2008','Phase1'],
+    ['3','IronMan2','May7,2010','Phase1'],
+    ['4','Thor','May6,2011','Phase1'],
+    ['5','CaptainAmerica:TheFirstAvenger','July22,2011','Phase1'],
+    ['6',"Marvel'sTheAvengers",'May4,2012','Phase1'],
+    ['7','IronMan3','May3,2013','Phase2'],
+    ['8','Thor:TheDarkWorld','November8,2013','Phase2'],
+    ['9','CaptainAmerica:TheWinterSoldier','April4,2014','Phase2'],
+    ['10','GuardiansoftheGalaxy','August1,2014','Phase2'],
+    ['11','Avengers:AgeofUltron','May1,2015','Phase2'],
+    ['12','Ant-Man','July17,2015','Phase2'],
+    ['13','CaptainAmerica:CivilWar','May6,2016','Phase3'],
+    ['14','DoctorStrange','May5,2017','Phase3'],
+    ['15','GuardiansoftheGalaxyVol.2','May5,2017','Phase3'],
+    ['16','Spider-Man:Homecoming','July7,2017','Phase3'],
+    ['17','Thor:Ragnarok','November3,2019','Phase3'],
+    ['18','BlackPanther','February16,2018','Phase3'],
+    ['19','Avengers:InfinityWar','April27,2018','Phase3'],
+    ['20','Ant-ManandtheWasp','July6,2018','Phase3'],
+    ['21','CaptainMarvel','March8,2019','Phase3'],
+    ['22','Avengers:Endgame','April26,2019','Phase3']]
+
+insert_query="INSERT INTO Movies(ID,MOVIE,DATE,MCU_PHASE) VALUES (%s,%s,%s,%s)"
+
+for row in data:
+    cursor.execute(insert_query,row)
+
+cnx.commit()
+cursor.close()
+cnx.close()
---
+++ myNinthProject.py
@@ -0,0 +1,151 @@
+import mysql.connector
+import tkinter as tk
+from tkinter import ttk
+from tkinter import messagebox
+
+def add_button_clicked():
+    dialog = tk.Toplevel(window)
+    dialog.title("ADD ITEM")
+
+    dialog_frame=ttk.Frame(dialog,padding=20)
+    dialog_frame.pack()
+
+    id_label=ttk.Label(dialog_frame,text="ID: ")
+    id_label.grid(row=0,column=0,sticky=tk.W)
+    id_entry=ttk.Entry(dialog_frame)
+    id_entry.grid(row=0,column=1,pady=10)
+
+    movie_label=ttk.Label(dialog_frame,text="MOVIE: ")
+    movie_label.grid(row=1,column=0,sticky=tk.W)
+    movie_entry=ttk.Entry(dialog_frame)
+    movie_entry.grid(row=1,column=1,pady=10)
+
+    date_label = ttk.Label(dialog_frame,text="DATE: ")
+    date_label.grid(row=2,column=0,sticky=tk.W)
+    date_entry=ttk.Entry(dialog_frame)
+    date_entry.grid(row=2,column=1,pady=10)
+
+    mcu_phase_label=ttk.Label(dialog_frame,text="MCU PHASE: ")
+    mcu_phase_label.grid(row=3,column=0,sticky=tk.W)
+    mcu_phase_entry=ttk.Entry(dialog_frame)
+    mcu_phase_entry.grid(row=3,column=1,pady=10)
+
+    def ok_button_clicked():
+        id_value=id_entry.get()
+        movie_value=movie_entry.get()
+        date_value=date_entry.get()
+        mcu_phase_value=mcu_phase_entry.get()
+
+        if id_value and movie_value and date_value and mcu_phase_value:
+            item=[id_value,movie_value,date_value,mcu_phase_value]
+            insert_data(item)
+            messagebox.showinfo("SUCCESS! ","ITEM HAS BEEN ADDED TO THE DATABASE.")
+            dialog.destroy()
+        else:
+            messagebox.showerror("ERROR! ","PLEASE FILL IN ALL FIELDS!")
+
+    ok_button=ttk.Button(dialog_frame,text="OK",command=ok_button_clicked)
+    ok_button.grid(row=4,column=0,pady=10,padx=10)
+
+    def cancel_button_clicked():
+        dialog.destroy()
+
+    cancel_button=ttk.Button(dialog_frame,text="CANCEL",command=cancel_button_clicked)
+    cancel_button.grid(row=4,column=1,pady=10,padx=10)
+
+def list_all_button_clicked():
+    items=select_all_data()
+    textbox.delete("1.0",tk.END)
+    for item in items:
+        item_str=" ".join(str(element) for element in item)
+        textbox.insert(tk.END,item_str+" \n")
+
+def insert_data(item):
+    cnx=mysql.connector.connect(
+        host='localhost',
+        port=3310,
+        user='root',
+        password='my_password',
+        database='my_module')
+    cursor = cnx.cursor()
+    insert_query="INSERT INTO Movies(ID,MOVIE,DATE,MCU_PHASE) VALUES (%s,%s,%s,%s)"
+    cursor.execute(insert_query,tuple(item))
+    cnx.commit()
+    cursor.close()
+    cnx.close()
+
+def select_data_by_id(id):
+    cnx=mysql.connector.connect(
+        host='localhost',
+        port=3310,
+        user='root',
+        password='my_password',
+        database='my_module')
+    cursor = cnx.cursor()
+    select_query="SELECT * FROM Movies WHERE ID=%s"
+    cursor.execute(select_query,(id,))
+    item=cursor.fetchone()
+    cursor.close()
+    cnx.close()
+    return item
+
+def dropdown_selected(event):
+    selected_id=dropdown.get()
+    selected_item=select_data_by_id(selected_id)
+    textbox.delete("1.0",tk.END)
+    if selected_item:
+        selected_item_str=" ".join(str(element) for element in selected_item)
+        textbox.insert(tk.END,selected_item_str)
+
+def select_all_data():
+    cnx=mysql.connector.connect(
+        host='localhost',
+        port=3310,
+        user='root',
+        password='my_password',
+        database='my_module')
+    cursor=cnx.cursor()
+    select_query="SELECT * FROM Movies"
+    cursor.execute(select_query)
+    items=cursor.fetchall()
+    cursor.close()
+    cnx.close()
+    return items
+
+def select_all_ids():
+    cnx=mysql.connector.connect(
+        host='localhost',
+        port=3310,
+        user='root',
+        password='my_password',
+        database='my_module')
+    cursor=cnx.cursor()
+    select_query="SELECT ID FROM Movies"
+    cursor.execute(select_query)
+    ids=[str(id[0]) for id in cursor.fetchall()]
+    cursor.close()
+    cnx.close()
+    return ids
+
+window=tk.Tk()
+window.title("SE_226_20200601009_LAB")
+
+frame=ttk.Frame(window,padding=20)
+frame.pack()
+
+add_button=ttk.Button(frame,text="ADD",command=add_button_clicked)
+add_button.grid(row=0,column=0,pady=10)
+
+list_all_button=ttk.Button(frame,text="LIST ALL",command=list_all_button_clicked)
+list_all_button.grid(row=0,column=1,pady=10,padx=10)
+
+ids=select_all_ids()
+
+dropdown=ttk.Combobox(frame,values=ids)
+dropdown.grid(row=1,column=0,columnspan=2,pady=10)
+dropdown.bind("<<ComboboxSelected>>",dropdown_selected)
+
+textbox=tk.Text(frame)
+textbox.grid(row=2,column=0,columnspan=2,pady=10)
+
+window.mainloop()
